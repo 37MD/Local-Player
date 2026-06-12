2c1b382e2c08930d458506052c19494eb9e3a545
37MD <150575622+37MD@users.noreply.github.com>
2026-06-12 11:22:50 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,18 @@
 const CACHE = 'lv-v1';
-const URLS = ['/Local-Player/', '/Local-Player/index.html'];
 
 self.addEventListener('install', function(e) {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE).then(function(c) { return c.addAll(URLS); }).then(function() { return self.skipWaiting(); })
+    caches.open(CACHE).then(function(c) { return c.addAll(['./', './index.html']); })
   );
 });
 
 self.addEventListener('activate', function(e) {
-  e.waitUntil(self.clients.claim());
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
+    }).then(function() { return self.clients.claim(); })
+  );
 });
 
 self.addEventListener('fetch', function(e) {
